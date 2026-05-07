c8a808ee42f866a26c86338dbb4e343491816b3a
Ric Angelo Galo <r.galo.128086.tc@umindanao.edu.ph>
2026-05-07 23:25:42 +0800

    refactor: Optimize component imports and enhance suspense handling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import './App.css';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Forecast from './pages/Forecast';
-import Dashboard from './pages/Dashboard';
-import About from './pages/About';
 import RootLayout from './layouts/RootLayout';
-
 import ErrorBoundary from './components/ErrorBoundary';
+
+const Home = lazy(() => import('./pages/Home'));
+const Forecast = lazy(() => import('./pages/Forecast'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const About = lazy(() => import('./pages/About'));
 
 function App() {
   return (
     <ErrorBoundary>
-      <Router>
-        <Routes>
-          <Route element={<RootLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/forecast" element={<Forecast />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/about" element={<About />} />
-          </Route>
-        </Routes>
-      </Router>
+      <Suspense fallback={null}>
+        <Router>
+          <Routes>
+            <Route element={<RootLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/forecast" element={<Forecast />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/about" element={<About />} />
+            </Route>
+          </Routes>
+        </Router>
+      </Suspense>
     </ErrorBoundary>
   );
 }
